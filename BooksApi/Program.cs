49751cc3b7a8174ad01ad257cd06fecde3000b42
Mrondon10 <90924079+Mrondon10@users.noreply.github.com>
2025-02-12 10:18:14 -0400

    feat: add swagger configuration to Program.cs

--- BooksApi/Program.cs
+++ BooksApi/Program.cs
@@ -8,9 +8,21 @@ builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<IBookService, BookService>();
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Books API v1");
+});
+
 
 app.MapControllers();
 
 app.Run();
-
