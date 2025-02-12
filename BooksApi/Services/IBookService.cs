3a8c6198d2c73167b37e4f875782ffbbe437d100
Mrondon10 <90924079+Mrondon10@users.noreply.github.com>
2025-02-12 00:07:49 -0400

    feat: add Book model and BookService to consume external API

---
+++ BooksApi/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using BooksApi.Models;
+
+namespace BooksApi.Services
+{
+	public interface IBookService
+	{
+		Task<IEnumerable<Book>> GetBooks();
+		Task<Book?> GetBookById(int id);
+		Task<Book?> CreateBook(Book book);
+		Task<Book?> UpdateBook(int id, Book book);
+		Task<bool> DeleteBook(int id);
+	}
+}
